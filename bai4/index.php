aec1b7a5ce7aa64ff10b833c3ecb95f14106904e
AnhMinh245 <AT160148@actvn.edu.vn>
2023-09-08 17:47:47 +0700

    first commit

---
+++ bai4/index.php
@@ -0,0 +1,73 @@
+<?php
+include('./pages/head.php');
+include('./pages/left.php');
+include('./pages/center.php');
+include('./pages/footer.php');
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- 
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>PHP và CSDL</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link type="text/css" rel="stylesheet" href="./css/style.css" />
+</head>
+
+<body>
+    Header
+    <div id="header">
+        <h1>Công nghệ Web an toàn</h1>
+        <h2>Bài 4: PHP và CSDL</h2>
+
+    </div>
+
+    Navigation
+    <div id="nav">
+        <a href="#">Calculate</a>
+        <a href="#">Drawtable</a>
+        <a href="#">Register</a>
+        <a href="#">Contact</a>
+    </div>
+
+    Left Sidebar
+    <div id="sideBar">
+        sideBar
+    </div>
+
+
+
+    Main content
+    <div id="content">
+        content
+    </div>
+
+
+
+    Footer
+    <div id="footer">
+        footer
+    </div>
+</body>
+
+</html> -->
---
+++ bai4/pages/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <div id="footer">
+        <p>&copy; <?php echo date("Y"); ?> Anh Minh</p>
+    </div>
+</footer>
+</body>
+</html>
